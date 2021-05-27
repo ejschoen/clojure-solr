50b9f9c4c47687c265554c47abf642ae38d43733
ejschoen <ejschoen@i2kconnect.com>
2021-05-27 08:52:51 -0500

    Add suggest test

--- test/clojure_solr_test.clj
+++ test/clojure_solr_test.clj
@@ -456,15 +456,13 @@ The maxInspections parameter defines the maximum number of possible matches to r
 
 At first, spellchecker analyses incoming query words by looking up them in the index. Only query words, which are absent in index or too rare ones (below maxQueryFrequency ) are considered as misspelled and used for finding suggestions. Words which are frequent than maxQueryFrequency bypass spellchecker unchanged. After suggestions for every misspelled word are found they are filtered for enough frequency with thresholdTokenFrequency as boundary value. These parameters (maxQueryFrequency and thresholdTokenFrequency) can be a percentage (such as .01, or 1%) or an absolute value (such as 4)."}))
   (commit!)
-  (search "Levenstein"
-          :rows 0
-          :spellcheck true
-          :spellcheck.build true
-          :df "fulltext"
-          :request-handler "/spell"
-          :response-handler (fn [_ r _] r))
-  (is (= {:collated-result "Levenshtein" :alternatives '("Levenshtein")}
-         (spellcheck "Levenstein" {:df "fulltext"}))))
+  (let [result (suggest "Leven" {:df "fulltext"})]
+    (println (format "Best suggestion for \"Leven\": %s" (:term (first result))))
+    (is (= (:term (first result) "Levenshtein"))))
+  (let [result (spellcheck "Levenstein" {:df "fulltext"})]
+    (println (format "Corrected Levenstein to %s" (:collated-result result)))
+    (is (= {:collated-result "Levenshtein" :alternatives '("Levenshtein")}
+           result))))
 
 
   
